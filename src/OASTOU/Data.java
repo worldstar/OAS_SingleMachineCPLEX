0da33476165c09c319167da67f3bcee4499b4fc0
worldstar <worldstar.chen@gmail.com>
2019-04-10 11:54:11 +0800

    Manage data parts
    We move the read data method into Class Data. Some electricity costs
    are added for each period.

--- src/OASTOU/Data.java
+++ src/OASTOU/Data.java
@@ -1,5 +1,10 @@
 package OASTOU;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
 //定義參數
 class Data{
 	int[][] arcs; //arcs[i][j]表示i到j點的弧
@@ -16,4 +21,76 @@ class Data{
 	//TOU
 	double[] EC;         //Electricity cost	
 	double[] intervalEndTime;  //The interval time
+	double[] unitPowerConsumption;
+
+	// 函数功能：從txt文件中讀取資料並初始化参数
+	public static void process_OAS(String path, Data data, int jobs) throws Exception {
+		// 初始化参数
+		FileInputStream fis = new FileInputStream(path);
+		DataInputStream in = new DataInputStream(fis);
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		String[] tmp;
+		data.jobs = jobs;
+		data.releaseTime = new int[jobs];
+		data.processingTime = new int[jobs];
+		data.dueDay = new int[jobs];
+		data.deadline = new int[jobs];
+		data.profit = new int[jobs];
+		data.weight = new double[jobs];
+		data.setup = new double[jobs][jobs];
+		data.arcs = new int[jobs][jobs];
+		
+		//TOU data
+		data.intervalEndTime = new double[] {420, 480, 690, 1110, 1380, 1440};
+		data.EC = new double[] {0.443, 0.8451, 1.2447, 0.8451, 1.2447, 0.443};
+		
+
+		tmp = br.readLine().split(",");// split 0,10,10,2,4,6,4,5,7,3,5,0
+		for (int i = 0; i < jobs-1; i++) { // i = orders,test 10
+			data.releaseTime[i+1] =  Integer.parseInt(tmp[i + 1]);
+		}
+
+		tmp = br.readLine().split(",");// split 0,10,10,2,4,6,4,5,7,3,5,0
+		for (int i = 0; i < jobs-1; i++) { // i = orders,test 10
+			data.processingTime[i+1] = Integer.parseInt(tmp[i + 1]);
+		}
+
+		tmp = br.readLine().split(",");// split 0,10,10,2,4,6,4,5,7,3,5,0
+		for (int i = 0; i < jobs-1; i++) { // i = orders,test 10
+			data.dueDay[i+1] = Integer.parseInt(tmp[i + 1]);
+		}
+
+		tmp = br.readLine().split(",");// split 0,10,10,2,4,6,4,5,7,3,5,0
+		for (int i = 0; i < jobs-1; i++) { // i = orders,test 10
+			data.deadline[i+1] = Integer.parseInt(tmp[i + 1]);
+		}
+
+		tmp = br.readLine().split(",");// split 0,10,10,2,4,6,4,5,7,3,5,0
+		for (int i = 0; i < jobs-1; i++) { // i = orders,test 10
+			data.profit[i+1] = Integer.parseInt(tmp[i + 1]);
+		}
+
+		tmp = br.readLine().split(",");// split 0,10,10,2,4,6,4,5,7,3,5,0
+		for (int i = 0; i < jobs-1; i++) { // i = orders,test 10
+			data.weight[i+1] = Double.parseDouble(tmp[i + 1]);
+		}
+
+		for (int i = 0; i < jobs ; i++) {
+			tmp = br.readLine().split(",");
+			for (int j = 0; j < jobs; j++) {
+				data.setup[i][j] = Integer.parseInt(tmp[j]);
+			}
+		}
+		
+		for (int i = 0; i < jobs ; i++) {
+			for (int j = 0; j < jobs; j++) {
+				if(i != j) {
+					data.arcs[i][j] = 1;
+				}
+				else {
+					data.arcs[i][j] = 0;
+				}
+			}
+		}													
+	}	
 }
--- src/OASTOU/OASTOUCplex.java
+++ src/OASTOU/OASTOUCplex.java
@@ -391,7 +391,7 @@ public class OASTOUCplex {
 		for(int i= 1; i < data.jobs-1;i++){//i=1,...,n	
 			IloNumExpr expr = model.diff(model.prod(data.profit[i], I[i]), model.prod(T[i], data.weight[i]));
 			for(int k = 0 ; k < data.intervalEndTime.length; k ++) {	
-				expr = model.diff(expr, model.prod(x[i][k], data.EC[k]));
+				expr = model.diff(expr, model.prod(x[i][k], data.EC[k]*data.unitPowerConsumption[i]));
 			}			
 			model.addLe(R[i], expr, "Eq9-TOU");//Ri<=reveneuei*Ii-Ti*weighti
 		}			
@@ -418,77 +418,13 @@ public class OASTOUCplex {
 //		}				
 }	
 	
-	// 函数功能：從txt文件中讀取資料並初始化参数
-	public static void process_OAS(String path, Data data, int jobs) throws Exception {
-		// 初始化参数
-		FileInputStream fis = new FileInputStream(path);
-		DataInputStream in = new DataInputStream(fis);
-		BufferedReader br = new BufferedReader(new InputStreamReader(in));
-		String[] tmp;
-		data.jobs = jobs;
-		data.releaseTime = new int[jobs];
-		data.processingTime = new int[jobs];
-		data.dueDay = new int[jobs];
-		data.deadline = new int[jobs];
-		data.profit = new int[jobs];
-		data.weight = new double[jobs];
-		data.setup = new double[jobs][jobs];
-		data.arcs = new int[jobs][jobs];
 
-		tmp = br.readLine().split(",");// split 0,10,10,2,4,6,4,5,7,3,5,0
-		for (int i = 0; i < jobs-1; i++) { // i = orders,test 10
-			data.releaseTime[i+1] =  Integer.parseInt(tmp[i + 1]);
-		}
-
-		tmp = br.readLine().split(",");// split 0,10,10,2,4,6,4,5,7,3,5,0
-		for (int i = 0; i < jobs-1; i++) { // i = orders,test 10
-			data.processingTime[i+1] = Integer.parseInt(tmp[i + 1]);
-		}
-
-		tmp = br.readLine().split(",");// split 0,10,10,2,4,6,4,5,7,3,5,0
-		for (int i = 0; i < jobs-1; i++) { // i = orders,test 10
-			data.dueDay[i+1] = Integer.parseInt(tmp[i + 1]);
-		}
-
-		tmp = br.readLine().split(",");// split 0,10,10,2,4,6,4,5,7,3,5,0
-		for (int i = 0; i < jobs-1; i++) { // i = orders,test 10
-			data.deadline[i+1] = Integer.parseInt(tmp[i + 1]);
-		}
-
-		tmp = br.readLine().split(",");// split 0,10,10,2,4,6,4,5,7,3,5,0
-		for (int i = 0; i < jobs-1; i++) { // i = orders,test 10
-			data.profit[i+1] = Integer.parseInt(tmp[i + 1]);
-		}
-
-		tmp = br.readLine().split(",");// split 0,10,10,2,4,6,4,5,7,3,5,0
-		for (int i = 0; i < jobs-1; i++) { // i = orders,test 10
-			data.weight[i+1] = Double.parseDouble(tmp[i + 1]);
-		}
-
-		for (int i = 0; i < jobs ; i++) {
-			tmp = br.readLine().split(",");
-			for (int j = 0; j < jobs; j++) {
-				data.setup[i][j] = Integer.parseInt(tmp[j]);
-			}
-		}
-		
-		for (int i = 0; i < jobs ; i++) {
-			for (int j = 0; j < jobs; j++) {
-				if(i != j) {
-					data.arcs[i][j] = 1;
-				}
-				else {
-					data.arcs[i][j] = 0;
-				}
-			}
-		}													
-	}
 	public static void main(String[] args) throws Exception {
 		Data data = new Data();
 		int jobs = 12;//所有點個數，包括0，n+1兩個虛擬訂單
 		//讀入不同的測試案例
 		String OASpath = "SingleMachineOAS/10orders/Tao1/R1/Dataslack_10orders_Tao1R1_1.txt";
-		process_OAS(OASpath,data,jobs);
+		data.process_OAS(OASpath,data,jobs);
 		System.out.println("input succesfully: \n"+OASpath);
 		System.out.println("cplex procedure###########################");
 		OASTOUCplex cplex = new OASTOUCplex(data);
